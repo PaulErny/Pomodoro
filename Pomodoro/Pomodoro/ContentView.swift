8647742f7a5b65c2051081da8464e5ed3fbeda4c
PaulErny <paul.erny@epitech.eu>
2024-03-01 10:35:29 +0100

    ADD - initial commit

---
+++ Pomodoro/Pomodoro/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pomodoro
+//
+//  Created by Paul Erny on 01/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pomodoro/Pomodoro/PomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroApp.swift
+//  Pomodoro
+//
+//  Created by Paul Erny on 01/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
